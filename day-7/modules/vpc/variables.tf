dd829871aa55ea5ce8eb1afce59c061040bc5bd0
Ubuntu <ubuntu@ip-172-31-15-23.ec2.internal>
2026-09-01 13:14:55 +0000

    added day 7

---
+++ day-7/modules/vpc/variables.tf
@@ -0,0 +1,16 @@
+variable "cidr" {
+  type = string
+}
+
+variable "public_subnet_cidr" {
+  type = string
+}
+
+variable "environment" {
+  type = string
+}
+
+variable "project_name" {
+  type = string
+}
+
